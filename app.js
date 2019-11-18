55041482978d8b17397f09a05e8044d96b01c385
ahmad zaaterah <39713415+zat0r@users.noreply.github.com>
2019-11-18 22:52:16 +0200

    moved to ejs

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-// requirements 
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
-// Pages Routers modules
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-// view engine setup
 var app = express();
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
