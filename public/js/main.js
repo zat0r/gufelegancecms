66e4726ef890d851cd711185e6180fae3fd4172a
ahmad zaaterah <39713415+zat0r@users.noreply.github.com>
2019-11-19 08:11:55 +0200

    ass style and fixe folder names

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,3 @@
-$(document).ready(function() {
-  PageData();
-    $(".closemodal").click(function() {
-        $("#Addnew").removeClass("animated bounceInDown shake");
-        $("#Addnew").addClass("animated bounceOutDown");
-        setTimeout(function(){
-            $("#Addnew").modal('hide');
-            $("#Addnew").removeClass("show animated bounceOutDown");
-    }, 1000);});
-    
-    $(".Openmodal").click(function() {
-      $("#Addnew").removeClass("animated bounceOutDown shake");
-      $("#Addnew").addClass("animated bounceInDown ");
-      $("#Addnew").addClass("show ");
-      setTimeout(function(){ $("#Addnew").removeClass("animated bounceInDown"); }, 1500);
-    
-    });
-});
 
 
 // ------ Validation Section ------ //
