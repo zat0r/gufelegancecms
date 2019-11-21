8defd8ea32805edcea40a6b51515b43abb08149a
ahmad zaaterah <39713415+zat0r@users.noreply.github.com>
2019-11-22 00:39:33 +0200

    added prodacts page

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var usersInfoRouter = require('./routes/userinfo');
-var apiRouter = require('./routes/api')
+var apiRouter = require('./routes/api');
+var proRouter = require('./routes/prodacts')
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/users/:userId', usersInfoRouter);
 app.use('/api', apiRouter);
+app.use('/prodacts', proRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
