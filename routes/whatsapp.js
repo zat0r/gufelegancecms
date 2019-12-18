9c48c94d614f0cf7bb3160b045237dbbeb7fd58a
ahmad zaaterah <39713415+zat0r@users.noreply.github.com>
2019-12-18 14:21:14 +0200

    other test

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -2,19 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-    console.log(req.botname);
-    console.log(req.messageobj)
-    var lol = {"messages": [
-          {
-            "id": "gBEGkZlgSYgQAgldNuJwtPwiviY"
-          }
-        ],
-        "meta": {
-          "api_status": "stable",
-          "version": "2.21.6"
-        }
-      } 
-      res.send(lol)
+    var massage = req.body
+    console.log(massage.botname);
+    console.log(massage.messageobj)
+    
+      res.send('lol')
   });
 
 module.exports = router;
