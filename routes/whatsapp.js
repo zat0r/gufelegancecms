0d29b9ec76313a02708235baa5915fd132d025df
ahmad zaaterah <39713415+zat0r@users.noreply.github.com>
2019-12-18 14:16:08 +0200

    fake replay

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -2,7 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-    console.log(req)
+    console.log(req.botname);
+    console.log(req.messageobj)
+    var lol = {"messages": [
+          {
+            "id": "gBEGkZlgSYgQAgldNuJwtPwiviY"
+          }
+        ],
+        "meta": {
+          "api_status": "stable",
+          "version": "2.21.6"
+        }
+      } 
+      res.send(lol)
   });
 
 module.exports = router;
