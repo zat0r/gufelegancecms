01314078c47e59ca7ca10b3f5ea784c9c97ad52c
ahmad zaaterah <39713415+zat0r@users.noreply.github.com>
2019-12-18 15:59:35 +0200

    lol

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
 
 router.post('/', function(req, res, next) {
-    var massage = req.body
-    console.log(massage.botname);
-    console.log(massage.messageobj)
-    
+    var massage = req.body.messageobj
+    console.log(massage.from);
+    console.log(massage.text);
+    if (massage.text === 'مرحبا'){
+      res.send('مرحبا بك في الرد الألي لأناقة الخليج ')
+    }
+    if (massage.text === 'عباية'){
+        res.send('يوجد لدينا تشكيلة ضخمة من العبايات')
+      }
+  });
+
+router.get('/', function(req, res, next) {
+    var query = req.query
+    console.log(query);
+    if (query.type === 'sendWAmassage'){
+
+    }
       res.send('lol')
   });
 
