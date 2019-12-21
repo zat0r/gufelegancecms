a8e93e5b9f98016ae7c34ffaf5eaa73a5e3ca363
ahmad zaaterah <39713415+zat0r@users.noreply.github.com>
2019-12-21 21:17:33 +0200

    WA

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var fs = require('fs');
+let rawdata = fs.readFileSync('WA.json');
+let WAData = JSON.parse(rawdata);
 var headers = {
   'Cache-Control': 'no-cache',
   'Content-Type': 'application/x-www-form-urlencoded',
@@ -9,24 +11,38 @@ var headers = {
   'cache-control': 'no-cache'
 };
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   var query = JSON.parse(req.body.messageobj)
-  console.log('messagefrom :' + query.from)
+  var SN = query.from.replace('962', '0')
+  var old = false;
+  console.log('messagefrom :' + SN)
   console.log('messagetext :' + query.text)
-
-  if(query.text === 'lol') {
-    res.send('loool')
+  for (var i = 0; i < WAData.length; i++) {
+    if (WAData[i].Number == SN) {
+      var old = true
+    }
   }
-  else{
+  if (old === false){
+    res.send('مرحبا بك في أناقة الخليج .. كيف ممكن أن أساعدك')
+    WAData += {Number: SN}
+    fs.writeFile('student-3.json', JSON.stringify(WAData), (err) => {
+      if (err) throw err;
+      console.log('Data written to file');
+  });
+  }
+  if (query.text === 'lol') {
+    res.send('loool')
+  }else {
     res.send('لم أفهم عليك')
   }
+  
 })
 
-router.get('/', function(req, res, next) {
-    var query = req.query
-    console.log(query);
-    if (query.type === 'sendWAmassage'){
-      var dataString = 'channel=whatsapp&source=917834811114&destination=962792880545&message=إختبار';
+router.get('/', function (req, res, next) {
+  var query = req.query
+  console.log(query);
+  if (query.type === 'sendWAmassage') {
+    var dataString = 'channel=whatsapp&source=917834811114&destination=962792880545&message=إختبار';
     var options = {
       url: 'https://api.gupshup.io/sm/api/v1/msg',
       method: 'POST',
@@ -35,14 +51,14 @@ router.get('/', function(req, res, next) {
     };
 
     request(options, callback);
+  }
+  else { res.send('lol') }
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+      res.send(body)
     }
-    else{res.send('lol')}
-    function callback(error, response, body) {
-      if (!error && response.statusCode == 200) {
-          console.log(body);
-          res.send(body)
-      }
-    }
-  });
-  
+  }
+});
+
 module.exports = router;
