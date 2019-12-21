652c7c6e1e5da6bdeb2adb57dcfa35fb86cd2e54
ahmad zaaterah <39713415+zat0r@users.noreply.github.com>
2019-12-21 11:48:42 +0200

    changed req to http

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -1,25 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request')
+var http = require("https");
+var options = {
+	"method": "POST",
+	"hostname": "api.gupshup.io",
+	"path": "/sm/api/v1/msg",
+	"headers": {
+		'Cache-Control': 'no-cache',
+    'apikey': 'fb5d3256b177450cc5981806105b696f',
+    'content-type': 'application/x-www-form-urlencoded'
+  }
+  
+};
+
+var WAreq = http.request(options, function (res) {
+	var chunks = [];
+
+	res.on("data", function (chunk) {
+		chunks.push(chunk);
+	});
 
 router.get('/', function(req, res, next) {
     var query = req.query
     console.log(query);
     if (query.type === 'sendWAmassage'){
-      var options = {
-        method: 'POST',
-        url: 'https://api.gupshup.io/sm/api/v1/msg',
-        headers: {
-          'Cache-Control': 'no-cache',
-          'apikey': 'fb5d3256b177450cc5981806105b696f',
-          'content-type': 'application/x-www-form-urlencoded'
-        },
-        data: {'channel': 'whatsapp', 'source': '917834811114', 'destination': '962792880545', 'message':'test'}
-      }
-      request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        console.log(body);
-      });
+      WAreq.write({'channel': 'whatsapp', 'source': '917834811114', 'destination': '962792880545', 'message':'test'})
     }
     else{res.send('lol')}
   });
