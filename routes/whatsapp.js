db81158b80c859f9521d30a5a5017cc2da656f3f
ahmad zaaterah <39713415+zat0r@users.noreply.github.com>
2019-12-21 13:27:43 +0200

    0

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -7,24 +7,6 @@ var header = {
   'apikey': 'fb5d3256b177450cc5981806105b696f',
   'content-type': 'application/x-www-form-urlencoded'
 }
-router.post('/', function(req, res, next) {
-    var massage = JSON.parse(req.body.messageobj)
-    console.log(massage.from);
-    console.log(massage.text);
-    if (massage.text === 'مرحبا'){
-      res.send('مرحبا بك في الرد الألي لأناقة الخليج ')
-    }
-    if (massage.text === 'عباية'){
-        res.send('يوجد لدينا تشكيلة ضخمة من العبايات')
-    }
-    if (massage.text === 'طلب'){
-        res.send('ماهي طلباتك')
-    }
-      
-      else{
-        res.send('لم أفهم عليك')
-      }
-  });
 
 router.get('/', function(req, res, next) {
     var query = req.query
