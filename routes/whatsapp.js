2c18339272125a98d98c4d4689372eab3a486f52
ahmad zaaterah <39713415+zat0r@users.noreply.github.com>
2019-12-21 11:39:29 +0200

    test without router post

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -2,25 +2,6 @@ var express = require('express');
 var router = express.Router();
 var request = require('request')
 
-router.post('/', function(req, res, next) {
-    var massage = JSON.parse(req.body.messageobj)
-    console.log(massage.from);
-    console.log(massage.text);
-    if (massage.text === 'مرحبا'){
-      res.send('مرحبا بك في الرد الألي لأناقة الخليج ')
-    }
-    if (massage.text === 'عباية'){
-        res.send('يوجد لدينا تشكيلة ضخمة من العبايات')
-    }
-    if (massage.text === 'طلب'){
-        res.send('ماهي طلباتك')
-    }
-      
-      else{
-        res.send('لم أفهم عليك')
-      }
-  });
-
 router.get('/', function(req, res, next) {
     var query = req.query
     console.log(query);
