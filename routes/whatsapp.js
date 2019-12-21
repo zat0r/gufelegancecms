da5a595b42f6a8149515a97db2e5320ec5322eb5
ahmad zaaterah <39713415+zat0r@users.noreply.github.com>
2019-12-21 12:50:23 +0200

    back to req

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -1,35 +1,44 @@
 var express = require('express');
 var router = express.Router();
-var http = require("https");
-var options = {
-	"method": "POST",
-	"hostname": "https://api.gupshup.io",
-	"path": "sm/api/v1/msg",
-	"headers": {
-		'Cache-Control': 'no-cache',
-    'apikey': 'fb5d3256b177450cc5981806105b696f',
-    'content-type': 'application/x-www-form-urlencoded'
-  }
-  
-};
+var request = require('request')
 
-var WAreq = http.request(options, function (res) {
-	var chunks = [];
-
-	res.on("data", function (chunk) {
-    chunks.push(chunk);
+router.post('/', function(req, res, next) {
+    var massage = JSON.parse(req.body.messageobj)
+    console.log(massage.from);
+    console.log(massage.text);
+    if (massage.text === 'مرحبا'){
+      res.send('مرحبا بك في الرد الألي لأناقة الخليج ')
+    }
+    if (massage.text === 'عباية'){
+        res.send('يوجد لدينا تشكيلة ضخمة من العبايات')
+    }
+    if (massage.text === 'طلب'){
+        res.send('ماهي طلباتك')
+    }
+      
+      else{
+        res.send('لم أفهم عليك')
+      }
   });
-  res.on("end", function () {
-		var body = Buffer.concat(chunks);
-		console.log(body.toString());
-	});
-});
 
 router.get('/', function(req, res, next) {
     var query = req.query
     console.log(query);
     if (query.type === 'sendWAmassage'){
-      WAreq.write(JSON.stringify({'channel': 'whatsapp', 'source': '917834811114', 'destination': '962792880545', 'message':'test'}))
+      var options = {
+        method: 'POST',
+        url: 'https://api.gupshup.io/sm/api/v1/msg',
+        headers: {
+          'Cache-Control': 'no-cache',
+          'apikey': 'fb5d3256b177450cc5981806105b696f',
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        body: JSON.stringify({channel: 'whatsapp', source: '917834811114', destination: '962792880545', message:'test'});
+      }
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        console.log(body);
+      });
     }
     else{res.send('lol')}
   });
